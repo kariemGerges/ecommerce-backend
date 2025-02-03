2bcde5c3e07af82ac89fc02e30f3eac3da396198
kariem <kiko1086@gmail.com>
2025-02-03 14:55:08 +0000

    OPTIONS 3

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const compression = require('compression');
-const rateLimit = require('express-rate-limit');
-const csurf = require('csurf');
-const redis = require('redis');
-const logger = require('morgan');
-const connectDB = require('./config/db');
-const sanitizeInput = require('./middlewares/sanitizeInput');
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const csurf = require("csurf");
+const redis = require("redis");
+const logger = require("morgan");
+const connectDB = require("./config/db");
+const sanitizeInput = require("./middlewares/sanitizeInput");
 // Route files
-const indexRouter = require('./routes/index');
-const authRoutes = require('./routes/authRouter');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
+const indexRouter = require("./routes/index");
+const authRoutes = require("./routes/authRouter");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 
 const app = express();
 
 // https://ecommerce-backend-8xv8.onrender.com (backend)
 // https://ecommerce-backend-8xv8.onrender.com/auth/login example
-
 
 // Connect to DB
 connectDB();
@@ -32,22 +31,24 @@ connectDB();
 // 1) SANITIZE all incoming data requests
 app.use(sanitizeInput);
 // 2) SECURITY: HELMET (SECURE HEADERS + HSTS)
-app.use(helmet({
-  // If needed customize certain headers, goes here. 
-  // Example: to allow images from your own domain:
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      imgSrc: ["'self'", 'data:', 'https:', 'http:'],
-      // etc...
+app.use(
+  helmet({
+    // If needed customize certain headers, goes here.
+    // Example: to allow images from your own domain:
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'", "data:", "https:", "http:"],
+        // etc...
+      },
     },
-  },
-  // Force HTTPS (HSTS) for maxTime = 60 days:
-  hsts: {
-    maxAge: 60 * 24 * 60 * 60, // 60 days in seconds
-    includeSubDomains: true,
-  },
-}));
+    // Force HTTPS (HSTS) for maxTime = 60 days:
+    hsts: {
+      maxAge: 60 * 24 * 60 * 60, // 60 days in seconds
+      includeSubDomains: true,
+    },
+  })
+);
 
 // 3) RATE LIMITING
 const limiter = rateLimit({
@@ -55,54 +56,55 @@ const limiter = rateLimit({
   // max: 200,                 // max requests per IP per windowMs
   message: {
     success: false,
-    message: 'Too many requests from this IP, please try again later.'
+    message: "Too many requests from this IP, please try again later.",
   },
 });
 app.use(limiter);
 
-// middleware 
-  const corsOptions = {
-    origin: [
-        'https://kariemgerges.github.io', // production frontend
-        'http://localhost:3000'           // local development frontend
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Length", "X-JSON"], // Optional: specify any headers that should be exposed
-    preflightContinue: false, // Pass to next middleware if options request is valid
-    optionsSuccessStatus: 204 // Status code for successful preflight response
+// middleware
+const corsOptions = {
+  origin: [
+    "https://ecommerce-frontend-qgyu.onrender.com", // production frontend
+    "https://ecommerce-frontend-henna-two.vercel.app", // production frontend
+    "https://kariemgerges.github.io", // production frontend
+    "http://localhost:3000", // local development frontend
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Length", "X-JSON"], // Optional: specify any headers that should be exposed
+  preflightContinue: false, // Pass to next middleware if options request is valid
+  optionsSuccessStatus: 204, // Status code for successful preflight response
 };
 
 app.use(cors(corsOptions));
 
-
 app.use(compression());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
 
 // Routes
-app.use('/', indexRouter);
-app.use('/auth', authRoutes);
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-app.use('/payment', paymentRoutes);
+app.use("/", indexRouter);
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/payment", paymentRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
